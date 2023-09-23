df82d3a0c7e811e4388f124584e0b6a9644f6154
iballi2017 <iballi201@gmail.com>
2023-09-23 23:01:03 +0100

    implement toastrs

--- src/scripts/main.jquery.js
+++ src/scripts/main.jquery.js
@@ -32,5 +32,42 @@ $(document).ready(function () {
     }
   });
 
-  
+
+
+  // TOASTR
+  toastr.options = {
+    'closeButton': true,
+    'debug': false,
+    'newestOnTop': false,
+    'progressBar': false,
+    'positionClass': 'toast-top-right',
+    'preventDuplicates': false,
+    'showDuration': '1000',
+    'hideDuration': '1000',
+    'timeOut': '5000',
+    'extendedTimeOut': '1000',
+    'showEasing': 'swing',
+    'hideEasing': 'linear',
+    'showMethod': 'fadeIn',
+    'hideMethod': 'fadeOut',
+  }
+
+});
+
+
+
+
+// Toast Type
+$('#success').click(function (event) {
+  toastr.success('You clicked Success toast');
+});
+$('#info').click(function (event) {
+  toastr.info('You clicked Info toast')
+});
+$('#error').click(function (event) {
+  toastr.error('You clicked Error Toast')
+});
+$('#warning').click(function (event) {
+  toastr.options.progressBar = true,
+  toastr.warning('You clicked Warning Toast')
 });
